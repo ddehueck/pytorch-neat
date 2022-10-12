5e2cf51aab87fc2776b7354621fc52c12fbd3b56
Gregory Conrad <gregorysconrad@gmail.com>
2022-10-12 17:14:37 -0400

    Added basic one-man-one vote ensemble network

---
+++ neat/phenotype/ensemble_feed_forward.py
@@ -0,0 +1,13 @@
+from neat.phenotype.feed_forward import FeedForwardNet
+import numpy as np
+
+
+class EnsembleFeedForward():
+    def __init__(self, genomes, config):
+        self.phenotypes = [FeedForwardNet(genome, config)
+                           for genome in genomes]
+        self.config = config
+
+    def __call__(self, input):
+        results = [phenotype(input) for phenotype in self.phenotypes]
+        return np.mean(results, axis=0)
