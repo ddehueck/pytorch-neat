26e3a756ddadcdfc7d9b89bd9461f8c21b85ac9b
Gregory Conrad <gregorysconrad@gmail.com>
2022-10-28 12:24:59 -0400

    Ensemble picker for picking size k ensembles from a list of genomes (#5)
    * Added ensemble picker method
    
    * Fixed the ensemble picker
    
    * Added ensemble generator convenience method
    
    * Added some docstrings and a bug fix
    
    * Create settings.json
    
    Co-authored-by: aidanhor-n <107128627+aidanhor-n@users.noreply.github.com>

--- neat/utils.py
+++ neat/utils.py
@@ -1,5 +1,7 @@
 import logging
 import copy
+import math
+import random
 
 import torch
 import numpy as np
@@ -38,6 +40,20 @@ def get_best_genome(population):
 
 
 def cache_genomes_results(genomes, dataset, config):
+    '''
+    Creates a prediction mapping of genomes to their 2D arrays of predictions
+
+    Each array in the 2D array is the prediction (activation outputs) for the given input
+    There is one array in each 2D array for each input in the dataset
+
+    Parameters:
+        genomes (iterable): the population of genomes
+        dataset (list of tensors): the list of tensor inputs
+        config (neat.config): the neat configuration
+
+    Returns:
+        dict of genome to the genome's predictions
+    '''
     genomes_to_results = {}
     for genome in genomes:
         results = []
@@ -49,3 +65,53 @@ def cache_genomes_results(genomes, dataset, config):
             results.append(prediction.numpy())
         genomes_to_results[genome] = np.array(results)
     return genomes_to_results
+
+
+def ensemble_generator(genomes, k=None, limit=None):
+    '''
+    A generator that randomly picks an ensemble from the given genomes of length k
+
+    Parameters:
+        genomes (iterable): the whole population of genomes to sample from
+        k (None | int): None (for random size ensembles) or the size of ensembles to create 
+        limit: the number of ensembles to yield before iterable exhaustion
+
+    Yields:
+       A set of genomes to use in an ensemble
+    '''
+    genomes = list(genomes)
+    n = len(genomes)
+    seen = set()
+
+    if limit is None:
+        limit = 2**n - 1 if k is None else math.comb(n, k)
+        limit /= 2
+
+    while len(seen) < limit:
+        ensemble_length = random.randint(1, n) if k is None else k
+        all_indices = list(range(n))
+        random.shuffle(all_indices)
+        ensemble_indices = frozenset(all_indices[0:ensemble_length])
+        if ensemble_indices not in seen:
+            seen.add(ensemble_indices)
+            ensemble = {genomes[i] for i in ensemble_indices}
+            yield ensemble
+
+
+def ensemble_generator_for_static_genome(genome, genomes, k=None, limit=None):
+    '''
+    A generator that randomly picks the rest of an ensemble of size k including a given genome
+
+    Parameters:
+        genome: the genome to include in the resulting ensembles
+        genomes (iterable): the whole population of genomes to sample from
+        k (None | int): None (for random size ensembles) or the size of ensembles to create 
+        limit: the number of ensembles to yield before iterable exhaustion
+
+    Yields:
+        A set of genomes to use in an ensemble
+    '''
+    genomes = [g for g in genomes if g != genome]
+    k = None if k is None else k - 1
+    for ensemble in ensemble_generator(genomes=genomes, k=k, limit=limit):
+        yield {genome, *ensemble}
