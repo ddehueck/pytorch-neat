4f78fafb4477ecd26715d7bd944e1b74684d22ef
Gregory Conrad <gregorysconrad@gmail.com>
2023-01-13 13:29:04 -0500

    Add the trial runner (#14)
    * Added the trial runner
    
    * Fixed path for output

---
+++ neat/experiments/template/main.py
@@ -0,0 +1,26 @@
+import json
+import os
+import sys
+
+from neat.analysis import run_trial_analysis
+from neat.experiments.template.config import TemplateConfig
+from neat.population import Population
+
+workdir = sys.argv[1]
+params_file = os.path.join(workdir, "params.json")
+
+with open(params_file, "r") as f:
+    # Set up this trial
+    params = json.load(f)
+    config = TemplateConfig(**params)
+    p = Population(config)
+
+    #  Run the trial itself
+    solution, generation = p.run()
+    final_pop = p.population
+
+    # Run analysis on the trial
+    pred_map = config.create_activation_map(final_pop)
+    ensemble_evaluator = config.ensemble_activations_evaluator
+    df = run_trial_analysis(pred_map, ensemble_evaluator)
+    df.to_csv(os.path.join(workdir, 'analysis_results.csv'), encoding='utf-8')
