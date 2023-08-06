8ee72f80e2d9346a41097c234008d38a3770c593
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2023-08-06 21:23:35 +0200

    Add relu to activations

--- neat/activations.py
+++ neat/activations.py
@@ -11,12 +11,17 @@ def tanh(x):
     return torch.tanh(x)
 
 
+def relu(x):
+    return torch.relu(x)
+
+
 class Activations:
 
     def __init__(self):
         self.functions = dict(
             sigmoid=sigmoid,
-            tanh=tanh
+            tanh=tanh,
+            relu=relu
         )
 
     def get(self, func_name):
